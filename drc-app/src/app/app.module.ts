527b2c0757530bf1e528db861ab709e78ef41f0f
vctrwesley <vctrwesley@gmail.com>
2025-09-05 12:46:11 -0300

    feat: modulos e arquitetura padrão

--- drc-app/src/app/app.module.ts
+++ drc-app/src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { LayoutComponent } from './layout/layout.component';
+import { SistemaModule } from './sistema/sistema.module';
+import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { EsqueciSenhaComponent } from './recuperar-senha/esqueci-senha/esqueci-senha.component';
+import { ResetPasswordComponent } from './recuperar-senha/reset-password/reset-password.component';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    EsqueciSenhaComponent,
+    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TemplateModule,
+    SistemaModule,
+    SharedModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
